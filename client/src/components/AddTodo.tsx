5389d69f793dd4d1ca75804c09e27f2890635d13
paul <paul.yang@turingchain.tech>
2022-12-26 22:59:02 -0500

    feat: add client side initial settings

---
+++ client/src/components/AddTodo.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import uuid from 'react-uuid';
+import { Todo } from '../interfaces/Todo.interface'
+
+interface Props {
+    dataUpdate: boolean
+    setDataUpdate: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const AddTodo: React.FC<Props> = ({ dataUpdate, setDataUpdate }) => {
+
+    const [thingsTodo, setThingsTodo] = useState<string>("")
+    const [priorityLevel, setPriorityLevel] = useState<string>("high")
+
+    const postTodo = (data: Todo) => {
+        const todo = data
+        fetch("/api", { method: "post", headers: { "Content-Type": "application/json" }, body: JSON.stringify(todo) })
+            .then(response => console.log("Response", response))
+            .catch(error => {
+                console.log("Error", error)
+            })
+    }
+
+    const selectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        setPriorityLevel(value);
+    };
+
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        const newTodo = {
+            name: thingsTodo, id: uuid(), priority: priorityLevel, completed: false
+        }
+        postTodo(newTodo)
+        console.log(dataUpdate)
+        setDataUpdate(!dataUpdate)
+        console.log(dataUpdate)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="thingsTodo">Things to do:&nbsp; </label>
+                <input type="text" id="thingsTodo" value={thingsTodo} onChange={(e) => setThingsTodo(e.target.value)} required />
+                <label htmlFor="priority">&nbsp; &nbsp;  Priority Level:&nbsp; </label>
+                <select id="priority" value={priorityLevel} onChange={selectChange} >
+                    <option value="high">High</option>
+                    <option value="alarming">Alarming</option>
+                    <option value="low">Low</option>
+                </select>
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
+
+export default AddTodo
